15756fa8b9c715a7081046d78b058cd7e9cddb5f
Arvind Sangwan <arvindsangwan@pm.me>
2025-07-06 02:57:45 +0530

    Update logger.dart

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,8 +1,43 @@
-import 'package:logger/logger.dart';
+import 'package:flutter/foundation.dart';
 
-/// Logger for the app.
-Logger mapLogger = Logger(
-  /// Logger level.
-  printer: PrettyPrinter(),
-  level: Level.trace,
-);
+class MyLogger {
+  final String tag;
+
+  MyLogger(this.tag);
+
+  void _log(String level, String message, {Object? error, StackTrace? stackTrace}) {
+    final timestamp = DateTime.now().toIso8601String();
+    final buffer = StringBuffer();
+
+    buffer.write("[$timestamp][$tag][$level]: $message");
+
+    if (error != null) {
+      buffer.write(" | Error: $error");
+    }
+    if (stackTrace != null) {
+      buffer.write("\nStackTrace: $stackTrace");
+    }
+
+    if (kDebugMode) {
+      print(buffer.toString());
+    }
+  }
+
+  void t(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('TRACE', message, error: error, stackTrace: stackTrace);
+
+  void d(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('DEBUG', message, error: error, stackTrace: stackTrace);
+
+  void i(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('INFO', message, error: error, stackTrace: stackTrace);
+
+  void w(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('WARN', message, error: error, stackTrace: stackTrace);
+
+  void e(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('ERROR', message, error: error, stackTrace: stackTrace);
+
+  void f(String message, {Object? error, StackTrace? stackTrace}) =>
+      _log('FATAL', message, error: error, stackTrace: stackTrace);
+}
